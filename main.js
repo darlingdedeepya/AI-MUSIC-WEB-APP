27a6f49c8b06d24a98468e087a2b661ab2f3ecd5
darlingdedeepya <87213449+darlingdedeepya@users.noreply.github.com>
2023-03-29 17:51:44 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+song1="";
+song2="";
+
 function setup(){
-    canvas = createCanvas(600,530);
+    canvas=createCanvas(600,500);
     canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function preload(){
+    song1=loadSoud("music.mp3");
+    song2=loadSound("music2.mp3");
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500);
 }
