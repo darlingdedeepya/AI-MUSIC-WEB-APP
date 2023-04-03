d3e60429566df29cd328b83832d798b1be1e2da9
darlingdedeepya <87213449+darlingdedeepya@users.noreply.github.com>
2023-04-03 09:45:19 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+rightWrist_X=0;
+rightWrist_Y=0;
+leftWrist_X=0;
+leftWrist_y=0;
 
 function setup(){
     canvas=createCanvas(600,500);
@@ -7,6 +11,9 @@ function setup(){
 
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotposes);
 }
 
 function preload(){
@@ -16,4 +23,22 @@ function preload(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded(){
+    console.log("PoseNet Is Initialized")
+}
+
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+
+        leftWrist_X = results[0].pose.leftWrist.x;
+        leftWrist_Y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x =" + leftWrist_X + " leftWrist_y = " + leftWrist_Y);
+
+        rightWrist_X = results[0].pose.rightWrist.x;
+        rightWrist_Y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x =" + rightWrist_X + " rightWrist_y = " + rightWrist_Y);
+    }
 }
